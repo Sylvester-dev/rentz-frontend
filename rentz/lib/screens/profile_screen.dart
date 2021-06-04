1b151c5fc239bb6ffd19c05dfcb271f4cf5a0e13
peaceDude404 <lcs2019012@iiitl.ac.in>
2021-06-04 15:57:51 +0530

    profile screen added

---
+++ rentz/lib/screens/profile_screen.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        color: Colors.white,
+        child: Column(
+          children: [
+            Column(
+              children: [
+                Container(
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(100)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 20, bottom: 8),
+                    child: Image.asset('assets/images/frame.jpg'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Aman Singh',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Theme.of(context).primaryColor),
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('lcs2019012@iiitl.ac.in'),
+                    )),
+              ],
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12,right: 12),
+                    child: Divider(
+                      color: Colors.black45
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 25,bottom: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 12.0),
+                          child: Text('lcs2019012@iiitl.ac.in',style: TextStyle(fontWeight: FontWeight.bold),),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 12.0),
+                          child: GestureDetector(
+                            child: Icon(Icons.edit),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12,right: 12),
+                    child: Divider(
+                      color: Colors.black45
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 25,bottom: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 12),
+                          child: Text('Update Password',style: TextStyle(fontWeight: FontWeight.bold),),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 12),
+                          child: GestureDetector(child: Icon(Icons.lock)),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12,right: 12),
+                    child: Divider(
+                      color: Colors.black45,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 25,bottom: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 12),
+                          child: Text('Log Out',style: TextStyle(fontWeight: FontWeight.bold),),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 12),
+                          child: GestureDetector(child: Icon(Icons.logout),),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
