8712046c657d220771c37fc557293c4207c4fa7a
peaceDude404 <lcs2019012@iiitl.ac.in>
2021-06-06 13:07:00 +0530

    favorite database added

---
+++ rentz/lib/utils/database.dart
@@ -0,0 +1,38 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart';
+
+class SqlDb {
+  Future<sql.Database> database() async {
+    final dbpath = await sql.getDatabasesPath();
+    return sql.openDatabase(path.join(dbpath, 'rentz.db'),
+        onCreate: (sql.Database db, int version) async {
+      db.execute(
+          'CREATE TABLE favorite (id TEXT PRIMARY KEY)');
+    });
+  }
+   
+   Future<void> insert_row(String table,Map<String,Object> data) async {
+     final db = await database();
+     db.insert(table,data);
+   }
+
+   Future<void> delete_row(String Id) async {
+     final db = await database();
+     db.rawDelete('DELETE FROM favorite WHERE id = ${Id}');
+   }
+
+   Future<bool> isfavorite(String Id) async {
+     final db = await database();
+     var result = await db.rawQuery('SELECT EXIST(SELECT 1 FROM favorite WHERE id=$Id)');
+     int flag = Sqflite.firstIntValue(result);
+     if(flag==1)
+     return true;
+     else 
+     return false;
+   }
+}
+
+// string - id 
+// insertion and deletion 
+// query searching
